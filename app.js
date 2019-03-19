6de8a200be4855f46eb74f862939dfbca3b6d95a
Katherine Fleming <katherinemarief@gmail.com>
2019-03-19 13:54:57 -0500

    getting close to being done I think

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ $(document).ready(function () {
     var upper = $("#keyboard-upper-container");
     var lower = $("#keyboard-lower-container");
     upper.hide();
-    numberOfMistakes = 0;
-    //write Jquery to set up keyboard toggling--
-    //while shift key is held down, hide the lowercase keyboard and show uppercase
-    //when shift key is released, show the lowercase keyboard and hide uppercase
+    numberOfMistakes = 0; //start this at 0 and have the mistakes increment as incorrect keys are typed
+    cursor = 0 //where the letterplace starts
+    displaySentence(sentences);//see function below
+    var targetletter = (sentences[index]).charAt(cursor);//there is something wrong with this..
+    $("#target-letter").text(targetletter);
+    //holding down shift makes Caps keyboard appear 
     $(document).keydown(function (e) {
         var keyName = e.keyCode;
         if (keyName === 16) {
@@ -22,71 +24,63 @@ $(document).ready(function () {
             lower.hide();
         }
         $(document).keyup(function (e) {
+            var keyName = e.keyCode;
             if (keyName === 16) {
                 upper.hide();
                 lower.show();
             }
         })
     })
-    //the sentences in the provided array shold be displayed at the top of the page one at a time
-    //once the sentence has been completed, the next should appear
-    //there is already a div with id=sentence where these should be displayed
-    index = 0
-    cursor = 0
-    //display the currently expected letter in the center (div id=target-letter)
-    //when keys are pressed, they should be highlighted in the browser.
-    //letters should be matched with ascii code(corresponds with id in html)
-
-    displaySentence(sentences);
-    let targetletter = (sentences[index].charCodeAt(cursor));
-    $("#target-letter").text(sentences[index]);
-
+    //keypress function-- all of the following will happen when a key is pressed.
     $(document).keypress(function (e) {
-        let keyPressed = $("#" + e.which);
-        keyPressed.addClass(".highlight");
-
+        let keyPressed = $("#" + e.which); //define the key that was pressed by its ID in HTML
+        keyPressed.addClass("highlight");
         cursor++;
-        $("#yellow-block").animate({ // make this go quicker
+        $("#yellow-block").animate({ // moved yellow div to right with each click by increasing margin and setting to speed 100
             "marginLeft": "+=17.5px"
+        }, 100);
+        $(document).keyup(function (e) {
+            let keyPressed = $("#" + e.which);
+            keyPressed.removeClass("highlight");
         })
         if (e.which === targetletter) { //if we are typing the correct key...
             let correctkey = $("<span></span>").addClass("glyphicon glyphicon-ok");
-            correctkey.appendTo("#feedback"); {
-                if (sentences[index].length = targetletter) {//if you complete a sentence...
-                    index++;
-                }
+            correctkey.appendTo("#feedback");
 
-                if (index === sentences.length) {
-                    gameOver === true;
-                    wpm = Math.round(54 / minutes - 2 * numberOfMistakes)
-                    let gameovermsg = $("<span></span>").text("you ran out of words! your words per minute ="(wpm));
-                    $("#sentence").append(gameovermsg);
+            if (targetletter === (sentences[index].length)) {//if you complete a sentence...
+                index++;
+                $("#yellow-block")
+            } if (index === (sentences.length)) {//if you complete all sentences...
+                gameOver === true;
+                let starttime= 0
+                let endtime = new Date().getTime(); 
+                let minutes= (endtime - starttime)/60
+                wpm = Math.round(54 / minutes - 2 * numberOfMistakes) //have to define minutes?
+                let gameovermsg = $("<span></span>").text("you ran out of words! your words per minute ="(wpm));
+                $("#sentence").append(gameovermsg);
+                let button = $("<btn>Play Again?</btn>").appendto("#sentence");
+                button.click(function(){
+                    resetGame();
+                })
 
-                }
             }
-
         } else {
             let incorrectkey = $("<span></span>").addClass("glyphicon glyphicon-remove")
             incorrectkey.appendTo("#feedback");
             numberOfMistakes++;
+
         }
-    }).keyup(function (e) {
-        let keyPressed = $("#" + e.which);
-        keyPressed.removeClass(".highlight");
-
-
     })
+
     function gameOver() {
         // show score (correct key log and incorrect key log) 
         //create yes/no button, if yes- reset game, if no do nothing
     }
     function resetGame() {
-        //clear out ("#target-letter") and go back to first sentence
-        //clear out ("feedback")
-        //load sentence from array
-        displaySentence(sentences);
+        $("target-letter").text("") //clear out ("#target-letter") and go back to first sentence
+        $("#feedback").text("") //clear out ("feedback")
+        displaySentence(sentences);//load sentence from array
     }
-
     function displaySentence(sentences) {
         index = 0;
         $("#sentence").text(sentences[index]);
